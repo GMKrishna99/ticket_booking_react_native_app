e5321c7c4450c3c156de72ccb4a5d9d02a7cb2ff
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-10-07 20:55:52 +0530

    chore : Home screen component

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface HomeScreenProps { }
+
+const HomeScreen = (props: HomeScreenProps) => {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+});
