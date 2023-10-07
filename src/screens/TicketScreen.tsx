5973e830a4865dd30ccac05d339499be17e86322
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-10-07 20:57:21 +0530

    chore : Ticket_Screen component

---
+++ src/screens/TicketScreen.tsx
@@ -0,0 +1,20 @@
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface TicketScreenProps { }
+
+const TicketScreen = (props: TicketScreenProps) => {
+    return (
+        <View style={styles.container}>
+            <Text>TicketScreen</Text>
+        </View>
+    );
+};
+
+export default TicketScreen;
+
+const styles = StyleSheet.create({
+    container: {},
+});
