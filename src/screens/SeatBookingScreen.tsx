21c094e6dbedff89abb33d6a79757413d3b307e9
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-10-07 20:57:04 +0530

    chore : Seat_Booking_Screen component

---
+++ src/screens/SeatBookingScreen.tsx
@@ -0,0 +1,20 @@
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface SeatBookingScreenProps { }
+
+const SeatBookingScreen = (props: SeatBookingScreenProps) => {
+    return (
+        <View style={styles.container}>
+            <Text>SeatBookingScreen</Text>
+        </View>
+    );
+};
+
+export default SeatBookingScreen;
+
+const styles = StyleSheet.create({
+    container: {},
+});
